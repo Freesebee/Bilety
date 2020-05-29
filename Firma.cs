bf881d5a739781c261600d8dbbb16169abbbbbf1
FreezeBee <63885889+Freesebee@users.noreply.github.com>
2020-05-29 02:53:32 +0200

    Nowe porządki
    * Rozbudowanie Klient, Firma, Osoba.
    Utworzenie własnych wyjątków i ich obsługa.
    Implementacja metod związanych z modyfikacją Firm, Pasażerów oraz sprawdzanie ich poprawnośći.
    
    * Pozbycie się podziału obowiązków (sprawiał problemy)

--- Firma.cs
+++ Firma.cs
@@ -14,22 +14,36 @@ namespace Bilety
 
         public Firma(string KRS, string nazwa_firmy)
         {
+            BiletSystem.CzyNumer(KRS); //Sprawdzenie poprawności numeru KRS
             nr_KRS = KRS;
             nazwa = nazwa_firmy;
-            lista_klientow = null;
+            lista_klientow = new List<Osoba>();
         }
         ~Firma()
         {
-            lista_klientow.Clear();
+            if (lista_klientow!=null)lista_klientow.Clear();
             lista_klientow = null;
         }
         public void DodajKlienta(Osoba nowy_klient)
         {
-            if (!lista_klientow.Contains(nowy_klient)) lista_klientow.Add(nowy_klient);
+            if (!lista_klientow.Contains(nowy_klient)) 
+                lista_klientow.Add(nowy_klient);
         }
         public void UsunKlienta(Osoba dany_klient)
         {
-            if (lista_klientow.Contains(dany_klient)) lista_klientow.Remove(dany_klient);
+            if (lista_klientow.Contains(dany_klient)) 
+                lista_klientow.Remove(dany_klient);
+        }
+        public override bool CzyZawieraZnaki(string tekst)
+        {
+            if (nazwa.Contains(tekst)
+                || nr_KRS.Contains(tekst)) return true;
+            else return false;
+        }
+        public override bool CzyTenSamUnikalnyNr(string nr)
+        {
+            if (nr_KRS.Equals(nr)) return true;
+            else return false;
         }
         public override string ToString()
         {
--- Klient.cs
+++ Klient.cs
@@ -7,5 +7,7 @@ namespace Bilety
 {
     public abstract class Klient
     {
+        public virtual bool CzyZawieraZnaki(string tekst) { return false; }
+        public virtual bool CzyTenSamUnikalnyNr(string nr) { return false; }
     }
 }
